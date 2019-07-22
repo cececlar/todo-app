e638ab5c1886941a603518545255100be41b61c7
Cece Claridge <cece.claridge@gmail.com>
2019-07-22 16:46:35 -0400

    todo app day 1

---
+++ todo-app/src/components/EditTodo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EditTodo extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Edit To Do Component!</p>
+      </div>
+    )
+  }
+}
+
+export default EditTodo;
---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Todo List Component!</p>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
